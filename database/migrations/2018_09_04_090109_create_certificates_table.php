bd58b354044b9244ca58376424ff518a862324a4
aseel omar <aseelomar07@gmail.com>
2018-09-04 13:10:19 +0300

    add migration

---
+++ database/migrations/2018_09_04_090109_create_certificates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('university_name');
+            $table->integer('doctor_id');
+            $table->integer('graduation_year');
+            $table->boolean('active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+}
---
+++ database/migrations/2018_09_04_090339_create_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->time('hour_from');
+            $table->time('hour_to');
+            $table->integer('reservation_id');
+            $table->string('reservation_type');
+            $table->boolean('active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
